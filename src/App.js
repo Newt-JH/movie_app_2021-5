cfb131b71c2e4a682721ec62af666e5208145c3a
NEWT-JH <flozzang@naver.com>
2021-09-08 17:18:45 +0900

    20210908

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function Newt() {
+  return (
+    <div>
+
+      Hello React!!!!!!!!!!!!
+
+    </div>
+    
+  );
+
+  }
+
+
+export default Newt;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import ReactDOM from 'react-dom';
+import Newt from './App';
+//import newt from './App';
+
+ReactDOM.render(
+
+    <Newt />,
+  
+
+  document.getElementById('root')
+);
